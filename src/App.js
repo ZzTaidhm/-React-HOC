444f14f525cd53732dbbf995514956dfa4cbacca
Zitai <592732348@qq.com>
2019-02-25 15:53:35 +0800

    初始化项目

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          初始化项目
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
